e855dc3d53fbd3894c87712fe7c5337e1715c6d3
Romano Silva <romano.magacho@gmail.com>
2019-08-15 08:50:26 -0300

    Add language support to import script.  (#5)
    * Issue #4 : add language support to import script. Added packs with locale data
    
    * Fixed Asp issue with \v instead of \r (#6). Simplified locale information to hold only name (#4)
    
    * Simplified packs locale and removed '-' from zh languages (#4)
    
    * Added thai language (AoA, CotA exclusive cards are still in English) (#4)

--- scripts/import.js
+++ scripts/import.js
@@ -5,5 +5,6 @@ let converter = new KeyforgeApiToKeytekiConverter();
 
 converter.convert({
     pathToPackFile: path.join(process.cwd(), process.argv[2]),
-    cyclePrefix: process.argv[3]
+    cyclePrefix: process.argv[3],
+    language: ((process.argv.length > 4) ? process.argv[4] : 'en')
 }).then(() => console.info('complete!')).catch(err => console.info(err));
--- src/KeyforgeApiToKeytekiConverter.js
+++ src/KeyforgeApiToKeytekiConverter.js
@@ -41,12 +41,15 @@ function sleep(ms) {
 }
 
 class KeyforgeApiToKeytekiConverter {
-    async convert({ pathToPackFile, cyclePrefix }) {
+    async convert({ pathToPackFile, language, cyclePrefix }) {
+
+        console.log('Loading ' + language + ' cards...');
+
         let pack = JSON.parse(fs.readFileSync(pathToPackFile));
 
         this.cyclePrefix = cyclePrefix;
 
-        let cards = await this.getCards(pack);
+        let cards = await this.getCards(pack, language);
 
         cards.sort((a, b) => a.number < b.number ? -1 : 1);
 
@@ -56,22 +59,28 @@ class KeyforgeApiToKeytekiConverter {
         }
 
         pack.cards = cards;
+
         fs.writeFileSync(pathToPackFile, JSON.stringify(pack, null, 4) + '\n');
         console.log('Import of cards for', pack.name, 'has been completed.');
     }
 
-    async getCards(pack) {
+    async getCards(pack, language) {
         const pageSize = 50;
         const apiUrl = 'https://www.keyforgegame.com/api/decks/';
 
         console.info('Fetching the deck list...');
+
+        let packCardMap = pack.cards.reduce(function(map, obj) {
+            map[obj.number] = obj;
+            return map;
+        }, {});
 
         let response;
         let cards = {};
         let pageErrors = [];
 
         try {
-            response = await httpRequest(apiUrl, { json: true });
+            response = await httpRequest(apiUrl, { json: true, headers: { 'Accept-Language': language } });
         } catch(err) {
             console.info(err);
 
@@ -85,7 +94,7 @@ class KeyforgeApiToKeytekiConverter {
 
         for(let i = 1; i < totalPages; i++) {
             try {
-                response = await httpRequest(`${apiUrl}/?page=${i}&links=cards&page_size=${pageSize}&ordering=-date`, { json: true });
+                response = await httpRequest(`${apiUrl}/?page=${i}&links=cards&page_size=${pageSize}&ordering=-date`, { json: true, headers: { 'Accept-Language': language } });
             } catch(err) {
                 if(err.statusCode === 429) {
                     await sleep(100);
@@ -111,34 +120,64 @@ class KeyforgeApiToKeytekiConverter {
                     continue;
                 }
 
-                let newCard = {
-                    id: card.card_title.toLowerCase().replace(/[?.!",“”]/gi, '').replace(/[ '’]/gi, '-'),
-                    name: card.card_title,
-                    number: card.card_number,
-                    image: card.front_image,
-                    expansion: card.expansion,
-                    house: card.house.toLowerCase(),
-                    keywords: this.parseKeywords(card.card_text),
-                    traits: !card.traits ? [] : card.traits.split(' • ').map(trait => trait.toLowerCase()),
-                    type: card.card_type.toLowerCase(),
-                    rarity: card.rarity,
-                    amber: card.amber === '' ? 0 : parseInt(card.amber),
-                    armor: card.card_type.toLowerCase() === 'creature' ? (card.armor !== '' ? parseInt(card.armor) : 0) : null,
-                    power: card.power === '' ? null : parseInt(card.power),
-                    text: card.card_text
+                let newCard = null;
+
+                if (language === 'en')  {
+
+                    newCard = {
+                        id: card.card_title.toLowerCase().replace(/[?.!",“”]/gi, '').replace(/[ '’]/gi, '-'),
+                        name: card.card_title,
+                        number: card.card_number,
+                        image: card.front_image,
+                        expansion: card.expansion,
+                        house: card.house.toLowerCase(),
+                        keywords: this.parseKeywords(card.card_text),
+                        traits: !card.traits ? [] : card.traits.split(' • ').map(trait => trait.toLowerCase()),
+                        type: card.card_type.toLowerCase(),
+                        rarity: card.rarity,
+                        amber: card.amber === '' ? 0 : parseInt(card.amber),
+                        armor: card.card_type.toLowerCase() === 'creature' ? (card.armor !== '' ? parseInt(card.armor) : 0) : null,
+                        power: card.power === '' ? null : parseInt(card.power),
+                        text: card.card_text,
+                        locale: {
+                            'en': {
+                                name: card.card_title
+                            }
+                        }
+                    };
+                } else {
+                    // Append locale information
+                    newCard = packCardMap[card.card_number];
+
+                    if (!newCard.locale) {
+                        // Just a safe check, but since 'en' is supposed to be loaded first, locale
+                        // will already exist
+                        newCard.locale = [];
+                    }
+
+                    newCard.locale[language.replace('-', '')] = {
+                        name: card.card_title
+                    };
+
                 };
+
+                // Sort locale by key
+                newCard.locale = Object.keys(newCard.locale).sort().reduce((newLocale, currentValue) => {
+                      newLocale[currentValue] = newCard.locale[currentValue];
+                      return newLocale;
+                    }, {});
 
                 cards[card.card_number] = newCard;
             }
 
             if(Object.values(cards).length == pack.cardCount) {
-                console.info('Got all the cards we were expecting');
+                console.info(`Got all the cards we were expecting after ${i} pages`);
 
                 break;
             }
 
             if(i % 10 === 0) {
-                console.info(`Processed ${i} pages, ${totalPages - i} to go.  Have ${Object.values(cards).length} cards so far, expecting ${pack.cardCount}..`);
+                console.info(`Processed ${i} pages, ${totalPages - i} to go. Have ${Object.values(cards).length} cards so far, expecting ${pack.cardCount}`);
             }
         }
 
@@ -146,7 +185,7 @@ class KeyforgeApiToKeytekiConverter {
     }
 
     parseKeywords(text) {
-        let lines = text.split('\r');
+        let lines = text.split(/[\r\v]/);
         let potentialKeywords = [];
 
         for(let line of lines) {
